6e240be5d83979bd59c85877c528dcbe3efccadd
Steffan van der Werf <steffan.van.der.werf@student.nhlstenden.com>
2021-09-30 21:28:14 +0200

    Style aangepast dubbele css weg gehaald en login pagina gemaakt + css toegevoegd en voorpagina aangemaakt

---
+++ pages/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voorpagina</title>
+    <link rel="stylesheet" href="./assets/styles/main.css">
+    <link href="./assets/img/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+</body>
+</html>
