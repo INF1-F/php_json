be079fed5e1b108d5daab06edd6fe9873474a2d9
Moniquesbg <moniquesbg@gmail.com>
2021-10-06 17:46:10 +0200

    Feedback selectie
    hierbij heb ik de feedbackselectie gemaakt. Ik heb er niet veel verstand van en idk of ik het goed doe, excuus.

---
+++ pages/Feedbackselectie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="main.css"/>
+    </head>
+
+<body>
+
+<h2>contact</h2>
+
+<div class="col-12">
+ <div class="col-11"><a href=""><p>Vragen</p></a></div>
+ <div class="col-10"><a href=""><p>Feedback</p></a></div>
+ <div class="col-9"><a href=""><p>Klachten</p></a></div>
+</div> 
+
+
+</body>
+</html>
