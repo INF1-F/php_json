f5fdff28e4f761093c5a920eda833a6ebb1df5a8
Marijke Baas <89772966+FreyaMarijke@users.noreply.github.com>
2021-10-06 17:01:55 +0200

    Marijke: Versie 1 van de header is af en de bijbehorende css is bijgewerkt. De css waar ik mee moest werken vond ik compleet onbegrijpelijk, dus als we bij elkaar kunnen gaan zitten om die te bespreken, zou ik dat heel fijn vinden.

---
+++ header.php
@@ -0,0 +1,34 @@
+<html lang="nl">
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./assets/styles/main.css"
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <ul class="d-flex list-unstyled align-items-center">
+                    <li><img src="./assets/img/Stenden_logo_wit.svg"></li>
+                    <li class="header-link d-flex align-items-center justify-content-center">Nieuws</li>
+                    <li class="header-link d-flex align-items-center justify-content-center">FAQ</li>
+                    <li class="header-link d-flex align-items-center justify-content-center">Contact</li>
+                </ul>
+            </div>
+            <div class="col-2 justify-content-center d-flex align-items-center">
+                <div class="round justify-content-center align-items-center d-flex">
+                    <img src="./assets/img/logo.svg" alt="Logo van de site" class="main-logo">
+                </div>
+            </div>
+            <div class="col-5">
+                <ul class="d-flex justify-content-right list-unstyled align-items-center">
+                    <li class="header-link d-flex align-items-center justify-content-center">Uitloggen</li>
+                    <li class="header-link d-flex align-items-center justify-content-center">NL/EN</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+</body>
+</html>
