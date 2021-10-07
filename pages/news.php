8123416a42e45883bb6389080936e83f9b205e25
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2021-10-07 11:14:21 +0200

    Nieuwspagina gemaakt
    De nieuwe nieuws pagina heet news, de oude van Max heet news_old.

--- pages/news.php
+++ pages/news.php
@@ -1,50 +1,74 @@
-<?php
-
-include '../components/main.php';
-
-$news_items = getJsonContent('news');
-
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-     <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-    <title>news</title>
+    <title>Voorpagina</title>
+    <link rel="stylesheet" href="../assets/styles/main.css">
+    <link href="./assets/img/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <div class="container row col-12">
-
-    <?php
-    // loopt door alle nieuwsitems heen
-    // $key = id van nieuwsitem bijv: 1632480707
-    // $item = nieuws bericht zelf
-    foreach ($news_items as $key => $item) {
-    ?>
-        <div class="card col-3" style="width: 18rem;">
-        <img src="../<?=$item->image?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"><?= $item->title?></h5>
-                <!-- laat de datum van het nieuws bericht zien -->
-                <p><?= date('d/m/y', $key)?></p>
-                <p class="card-text"><?= $item->beschrijving ?></p>
-                <a href="./reaction.php?id=<?=$key?>" class="card-link">Reageer</a>
-                <a href="" class="card-link">Reacties</a>
+<body class="h-100">
+    <div class="container h-100">
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex h-100">
+                <h2>Nieuws</h2>
             </div>
         </div>
-    <?php
-
-    }
-    ?>
+        <div class="row mb-2">
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-flex paddingRightClear">
+                <div class="newstile col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex">
+                    <div style="background-image: url('../assets/img/news_items/NHLStendenEmmen.jpeg')" class="articleImage"></div>
+                </div>
+            </div>
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-flex paddingLeftClear">
+                <div class="newstile col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex">
+                    <div class="container">
+                        <h2 class="mt-2 mb-1"> Title </h2>
+                        <p class="mt-1 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec iaculis massa,
+                            non rhoncus enim. Sed tristique blandit odio dignissim tempor. Suspendisse at
+                            lorem ex. Pellentesque ornare elit nulla, id ultrices magna venenatis a.
+                            Quisque consequat dolor ante, semper cursus lacus gravida vitae. Aliquam mattis
+                            purus enim. Morbi laoreet vitae risus ac sodales. Aenean rutrum, neque porttitor
+                            euismod pulvinar, sem lacus posuere tellus, eu finibus orci metus quis diam.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-flex paddingRightClear">
+                <div class="newstile col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex">
+                    <div style="background-image: url('../assets/img/news_items/NHLStendenEmmen.jpeg')" class="articleImage"></div>
+                </div>
+            </div>
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-flex paddingLeftClear">
+                <div class="newstile col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex">
+                    <div class="container">
+                        <h2 class="mt-2 mb-1"> Title </h2>
+                        <p class="mt-1 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec iaculis massa,
+                            non rhoncus enim. Sed tristique blandit odio dignissim tempor. Suspendisse at
+                            lorem ex. Pellentesque ornare elit nulla, id ultrices magna venenatis a.
+                            Quisque consequat dolor ante, semper cursus lacus gravida vitae. Aliquam mattis
+                            purus enim. Morbi laoreet vitae risus ac sodales. Aenean rutrum, neque porttitor
+                            euismod pulvinar, sem lacus posuere tellus, eu finibus orci metus quis diam.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 d-flex h-100 justify-content-center">
+                <button type="button" class="btn2">Meer weergeven</button>
+            </div>
+        </div>
     </div>
-
 </body>
 
 </html>
