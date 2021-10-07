239150c2b9196609de8c7bf262b24d4711b73448
Marijke Baas <89772966+FreyaMarijke@users.noreply.github.com>
2021-10-07 11:33:37 +0200

    Marijke: header voor mobiel en tablet gemaakt, bijbehorende css bijgewerkt.

--- header.php
+++ header.php
@@ -8,7 +8,8 @@
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-5">
+<!--        Dit is het linkerdeel van de header voor laptopformaat en groter-->
+            <div class="col-lg-5 col-4 d-none d-lg-block">
                 <nav>
                     <ul class="d-flex list-unstyled align-items-center">
                         <li><img src="./assets/img/Stenden_logo_wit.svg" alt="Het logo van Stenden"></li>
@@ -18,16 +19,31 @@
                     </ul>
                 </nav>
             </div>
-            <div class="col-2 justify-content-center d-flex align-items-center">
+<!--        Dit is het linkerdeel van de header voor alles kleiner van laptopformaat, dus telefoon en tablet-->
+            <div class="col-lg-5 col-4 d-lg-none">
+                <a href="#" class="d-flex justify-content-center"><img src="./assets/img/logo_white.svg" alt="Logo van de site in wit"></a>
+                <a href="#" class="d-flex justify-content-center">MENU</a>
+            </div>
+<!--        Dit is het midden van de header voor laptopformaat en groter-->
+            <div class="col-lg-2 col-4 justify-content-center d-lg-flex align-items-center d-none">
                 <div class="round justify-content-center align-items-center d-flex">
                     <a href="#"><img src="./assets/img/logo.svg" alt="Logo van de site" class="main-logo"></a>
                 </div>
             </div>
-            <div class="col-5">
+<!--        Dit is het midden van de header voor telefoon en tablet-->
+            <div class="col-lg-2 col-4 d-flex justify-content-center d-lg-none">
+                <img src="./assets/img/Stenden_logo_wit.svg" alt="Het logo van Stenden">
+            </div>
+<!--        Dit is het rechterdeel van de header voor laptopformaat en groter-->
+            <div class="col-lg-5 col-4 d-none d-lg-block">
                 <ul class="d-flex justify-content-right list-unstyled align-items-center">
                     <li class="header-link d-flex align-items-center justify-content-center"><a href="#">Uitloggen</a></li>
                     <li class="header-link d-flex align-items-center justify-content-center"><a href="#">NL</a>/<a href="#">EN</a></li>
                 </ul>
+            </div>
+<!--        Dit is het rechterdeel van de header voor kleiner dan laptopformaat, dus telefoon en tablet-->
+            <div class="col-lg-5 col-4 d-lg-none d-flex align-items-center justify-content-center">
+                <a href="#">NL</a>/<a href="#">EN</a>
             </div>
         </div>
     </div>
