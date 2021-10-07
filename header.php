087d7a043372b77ca6cc202f01ae1ba2d81c2006
Marijke Baas <89772966+FreyaMarijke@users.noreply.github.com>
2021-10-07 09:49:30 +0200

    Marijke: nav tag toegevoegd, van de benodigde onderdelen linkjes gemaakt, bijbehorende css aangepast.

--- header.php
+++ header.php
@@ -9,22 +9,24 @@
     <div class="container">
         <div class="row">
             <div class="col-5">
-                <ul class="d-flex list-unstyled align-items-center">
-                    <li><img src="./assets/img/Stenden_logo_wit.svg"></li>
-                    <li class="header-link d-flex align-items-center justify-content-center">Nieuws</li>
-                    <li class="header-link d-flex align-items-center justify-content-center">FAQ</li>
-                    <li class="header-link d-flex align-items-center justify-content-center">Contact</li>
-                </ul>
+                <nav>
+                    <ul class="d-flex list-unstyled align-items-center">
+                        <li><img src="./assets/img/Stenden_logo_wit.svg"></li>
+                        <li class="header-link d-flex align-items-center justify-content-center"><a href="#">Nieuws</a></li>
+                        <li class="header-link d-flex align-items-center justify-content-center"><a href="#">FAQ</a></li>
+                        <li class="header-link d-flex align-items-center justify-content-center"><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
             </div>
             <div class="col-2 justify-content-center d-flex align-items-center">
                 <div class="round justify-content-center align-items-center d-flex">
-                    <img src="./assets/img/logo.svg" alt="Logo van de site" class="main-logo">
+                    <a href="#"><img src="./assets/img/logo.svg" alt="Logo van de site" class="main-logo"></a>
                 </div>
             </div>
             <div class="col-5">
                 <ul class="d-flex justify-content-right list-unstyled align-items-center">
-                    <li class="header-link d-flex align-items-center justify-content-center">Uitloggen</li>
-                    <li class="header-link d-flex align-items-center justify-content-center">NL/EN</li>
+                    <li class="header-link d-flex align-items-center justify-content-center"><a href="#">Uitloggen</a></li>
+                    <li class="header-link d-flex align-items-center justify-content-center"><a href="#">NL</a>/<a href="#">EN</a></li>
                 </ul>
             </div>
         </div>
