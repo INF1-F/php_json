a592cb17a5b61e1f6196f3c7876744590d383b69
max appeldorn <mappeldorn1@st.noorderpoort.nl>
2021-10-12 14:40:21 +0200

    created add-news.php

---
+++ pages/add-news.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php  include_once('../components/head.php')?>
+    <title>Voeg nieuws toe</title>
+</head>
+<body>
+    <?php include_once('../components/header.php')?>
+
+    
+
+    <?php include_once('../components/footer.php')?>
+</body>
+</html>
