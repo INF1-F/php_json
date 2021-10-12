3179be55b8293c37e11b03272cad713d712b5935
max appeldorn <mappeldorn1@st.noorderpoort.nl>
2021-10-12 15:07:54 +0200

    added hero img

--- pages/add-news.php
+++ pages/add-news.php
@@ -2,13 +2,17 @@
 <html lang="en">
 <head>
     <?php  include_once('../components/head.php')?>
+
     <title>Voeg nieuws toe</title>
 </head>
 <body>
     <?php include_once('../components/header.php')?>
 
-    
+    <div class="hero">
+        
+    </div>
 
-    <?php include_once('../components/footer.php')?>
+
+    <!-- <?php include_once('../components/footer.php')?> -->
 </body>
 </html>
