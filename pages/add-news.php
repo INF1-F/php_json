4623b5a23c468a25ee153ba642a8295267fdd4d4
max appeldorn <mappeldorn1@st.noorderpoort.nl>
2021-10-13 10:51:33 +0200

    id+names naar engels

--- pages/add-news.php
+++ pages/add-news.php
@@ -13,7 +13,7 @@
             <div class="col-12">
                 <h1>Voeg nieuws toe</h1>
             </div>
-            <div class="col-12 mb-3">
+            <div class="col-12 mb-3 add-news">
                 <div class="card align-items">
                     <div class="card-body">
                         <form class="contactForm" action="news.php" method="POST" enctype="multipart/form-data">
@@ -22,23 +22,24 @@
                                 <input type="text" id="title" name="title" class="form-control" placeholder="Typ hier uw titel">
                             </div>
                             <div class="form-row">
-                                <label for="myfile"><b>Selecteer een afbeelding:</b></label>
-                                <input type="file" class="form-control" id="myfile" name="myfile">
+                                <label for="file"><b>Selecteer een afbeelding:</b></label>
+                                <input type="file" class="form-control" id="file" name="file">
                             </div>
                             <div class="form-row">
-                                <label for="artikel"><b>Artikel</b></label>
-                                <textarea id="artikel" class="form-control" name="artikel" style="height: 200px;" placeholder="Typ hier uw artikel"></textarea>
+                                <label for="article"><b>Artikel</b></label>
+                                <textarea id="article" class="form-control" name="article" style="height: 200px;" placeholder="Typ hier uw artikel"></textarea>
                             </div>
                             <div class="form-row">
-                                <input class="float-right btn btn-primary" type="submit" value="Versturen">
                             </div>
                         </form>
                     </div>
                 </div>
+                <input class="float-right btn btn-primary" type="submit" value="Versturen">
+
             </div>
         </div>
     </div>
 
-    <!-- <?php include_once('../components/footer.php')?> -->
+    <?php include_once('../components/footer.php')?>
 </body>
 </html>
