d4163270effe09a35d1d69b54ef121899aa656a3
max appeldorn <mappeldorn1@st.noorderpoort.nl>
2021-10-14 14:45:44 +0200

    get news items

--- pages/news.php
+++ pages/news.php
@@ -1,3 +1,10 @@
+<?php
+
+include('../components/main.php');
+
+$news = getJsonContent('news');
+
+?>
 <!DOCTYPE html>
 <html lang="en" class="h-100">
 
@@ -22,6 +29,8 @@
                 ?>
             </div>
         </div>
+
+
         <div class="row mb-2 mt-2">
             <div class="col-12">
                 <div class="card">
@@ -47,31 +56,8 @@
                 </div>
             </div>
         </div>
-        <div class="row mb-2 mt-2">
-            <div class="col-12">
-                <div class="card">
-                    <div class="row">
-                        <div class="col-md-6 col-12">
-                            <div class="news-image h-100 w-100">
-                                <div style="background-image: url('../assets/img/news_items/NHLStendenEmmen.jpg')" class="articleImage h-100 w-100"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-12">
-                            <div class="news-description card-body">
-                                <h2 class="m-0">Titel</h2>
-                                <p class="mt-1 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec iaculis massa,
-                                    non rhoncus enim. Sed tristique blandit odio dignissim tempor. Suspendisse at
-                                    lorem ex. Pellentesque ornare elit nulla, id ultrices magna venenatis a.
-                                    Quisque consequat dolor ante, semper cursus lacus gravida vitae. Aliquam mattis
-                                    purus enim. Morbi laoreet vitae risus ac sodales. Aenean rutrum, neque porttitor
-                                    euismod pulvinar, sem lacus posuere tellus, eu finibus orci metus quis diam.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+       
+
         <div class="row mb-4">
             <div class="col-12 d-flex justify-content-center">
                 <button type="button" class="btn btn-custom">Meer weergeven</button>
