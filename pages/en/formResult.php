6fcc2085a09df3b533052bd836f1e3324c48a62e
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2021-10-29 13:20:49 +0200

    header already send waring opgelost

--- pages/en/formResult.php
+++ pages/en/formResult.php
@@ -1,5 +1,54 @@
 <?php
 session_start();
+
+if(isset($_POST['submit'])) {
+    if (isset($_POST['sort']) && isset($_POST['formtype'])) {
+        $sort = $_POST['sort'];
+        $formtype = $_POST['formtype'];
+        if(isset($_POST['education']) && !empty($_POST['education'])) {
+            $education = $_POST['education'];
+            if(isset($_POST['location']) && !empty($_POST['location'])) {
+                $location = $_POST['location'];
+                if(isset($_POST['message']) && !empty($_POST['message'])) {
+                    $message = $_POST['message'];
+                    if (isset($_POST['name']) && !empty($_POST['name']))  {
+                        $name = $_POST['name'];
+                    } else {
+                        $name = "Anoniem";
+                    }
+
+                    $file = fopen ('../../assets/data/contact/formData-'.date("YmdHis").'.txt', 'a');
+
+                    fwrite($file, "Type formulier:");
+                    fwrite($file, $sort . "\n");
+                
+                    fwrite($file, "naam:");
+                    fwrite($file, $name . "\n");
+                
+                    fwrite($file, "Type opleiding:");
+                    fwrite($file, $education . "\n");
+                
+                    fwrite($file, "Locatie:");
+                    fwrite($file, $location . "\n");
+                
+                    fwrite($file, "Bericht:");
+                    fwrite($file, $message . "\n");
+                
+                    fclose($file);
+                    $succesText = "<p>Thanks for sending! You will receive a reply as soon as possible!</p>";
+                } else {
+                    header("location: ./form.php?choise={$formtype}&error=message");
+                } 
+            } else {
+                header("location: ./form.php?choise={$formtype}&error=location");
+            } 
+        } else {
+            header("location: ./form.php?choise={$formtype}&error=education");
+        } 
+    }else {
+        header("location: ./form.php?choise={$formtype}&error=sort");
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,56 +62,7 @@ session_start();
             <div class="row">
                 <div class="col-12">
                     <div class="verzonden">
-                    <?php
-                    if(isset($_POST['submit'])) {
-                        if (isset($_POST['sort']) && isset($_POST['formtype'])) {
-                            $sort = $_POST['sort'];
-                            $formtype = $_POST['formtype'];
-                            if(isset($_POST['education']) && !empty($_POST['education'])) {
-                                $education = $_POST['education'];
-                                if(isset($_POST['location']) && !empty($_POST['location'])) {
-                                    $location = $_POST['location'];
-                                    if(isset($_POST['message']) && !empty($_POST['message'])) {
-                                        $message = $_POST['message'];
-                                        if (isset($_POST['name']) && !empty($_POST['name']))  {
-                                            $name = $_POST['name'];
-                                        } else {
-                                            $name = "Anoniem";
-                                        }
-
-                                        $file = fopen ('../../assets/data/contact/formData-'.date("YmdHis").'.txt', 'a');
-
-                                        fwrite($file, "Type formulier:");
-                                        fwrite($file, $sort . "\n");
-                                    
-                                        fwrite($file, "naam:");
-                                        fwrite($file, $name . "\n");
-                                    
-                                        fwrite($file, "Type opleiding:");
-                                        fwrite($file, $education . "\n");
-                                    
-                                        fwrite($file, "Locatie:");
-                                        fwrite($file, $location . "\n");
-                                    
-                                        fwrite($file, "Bericht:");
-                                        fwrite($file, $message . "\n");
-                                    
-                                        fclose($file);
-                                        echo "<p>Thanks for sending! You will receive a reply as soon as possible!</p>";
-                                    } else {
-                                        header("location: ./form.php?choise={$formtype}&error=message");
-                                    } 
-                                } else {
-                                    header("location: ./form.php?choise={$formtype}&error=location");
-                                } 
-                            } else {
-                                header("location: ./form.php?choise={$formtype}&error=education");
-                            } 
-                        }else {
-                            header("location: ./form.php?choise={$formtype}&error=sort");
-                        }
-                    }
-                    ?>
+                        <?=$succesText?>
                     </div>
                 </div>
             </div>
--- pages/nl/formResult.php
+++ pages/nl/formResult.php
@@ -1,5 +1,54 @@
 <?php
 session_start();
+
+if(isset($_POST['submit'])) {
+    if (isset($_POST['sort']) && isset($_POST['formtype'])) {
+        $sort = $_POST['sort'];
+        $formtype = $_POST['formtype'];
+        if(isset($_POST['education']) && !empty($_POST['education'])) {
+            $education = $_POST['education'];
+            if(isset($_POST['location']) && !empty($_POST['location'])) {
+                $location = $_POST['location'];
+                if(isset($_POST['message']) && !empty($_POST['message'])) {
+                    $message = $_POST['message'];
+                    if (isset($_POST['name']) && !empty($_POST['name']))  {
+                        $name = $_POST['name'];
+                    } else {
+                        $name = "Anoniem";
+                    }
+
+                    $file = fopen ('../../assets/data/contact/formData-'.date("YmdHis").'.txt', 'a'); //waneer men het formulier verzend, word een file opengemaakt waar de formsdata wordt opgeslagen.
+
+                    fwrite($file, "Type formulier:");
+                    fwrite($file, $sort . "\n");
+                
+                    fwrite($file, "naam:");
+                    fwrite($file, $name . "\n");
+                
+                    fwrite($file, "Type opleiding:");
+                    fwrite($file, $education . "\n");
+                
+                    fwrite($file, "Locatie:");
+                    fwrite($file, $location . "\n");
+                
+                    fwrite($file, "Bericht:");
+                    fwrite($file, $message . "\n");
+                
+                    fclose($file);
+                    $succesText = "<p>Bedankt voor het versturen! U krijgt zo spoedig mogelijk antwoord!</p>"; //waneer men het formulier verzend, komt er "bedankt voor het versturen te staan
+                } else {
+                    header("location: form.php?choise={$formtype}&error=message");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen message hebt ingevuld
+                } 
+            } else {
+                header("location: form.php?choise={$formtype}&error=location");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen locatie hebt ingevuld
+            } 
+        } else {
+            header("location: form.php?choise={$formtype}&error=education");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen opleiding hebt ingevuld
+        } 
+    }else {
+        header("location: form.php?choise={$formtype}&error=sort");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen soortformulier hebt ingevuld
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -13,56 +62,7 @@ session_start();
             <div class="row">
                 <div class="col-12">
                     <div class="verzonden">
-                    <?php
-                    if(isset($_POST['submit'])) {
-                        if (isset($_POST['sort']) && isset($_POST['formtype'])) {
-                            $sort = $_POST['sort'];
-                            $formtype = $_POST['formtype'];
-                            if(isset($_POST['education']) && !empty($_POST['education'])) {
-                                $education = $_POST['education'];
-                                if(isset($_POST['location']) && !empty($_POST['location'])) {
-                                    $location = $_POST['location'];
-                                    if(isset($_POST['message']) && !empty($_POST['message'])) {
-                                        $message = $_POST['message'];
-                                        if (isset($_POST['name']) && !empty($_POST['name']))  {
-                                            $name = $_POST['name'];
-                                        } else {
-                                            $name = "Anoniem";
-                                        }
-
-                                        $file = fopen ('../../assets/data/contact/formData-'.date("YmdHis").'.txt', 'a'); //waneer men het formulier verzend, word een file opengemaakt waar de formsdata wordt opgeslagen.
-
-                                        fwrite($file, "Type formulier:");
-                                        fwrite($file, $sort . "\n");
-                                    
-                                        fwrite($file, "naam:");
-                                        fwrite($file, $name . "\n");
-                                    
-                                        fwrite($file, "Type opleiding:");
-                                        fwrite($file, $education . "\n");
-                                    
-                                        fwrite($file, "Locatie:");
-                                        fwrite($file, $location . "\n");
-                                    
-                                        fwrite($file, "Bericht:");
-                                        fwrite($file, $message . "\n");
-                                    
-                                        fclose($file);
-                                        echo "<p>Bedankt voor het versturen! U krijgt zo spoedig mogelijk antwoord!</p>"; //waneer men het formulier verzend, komt er "bedankt voor het versturen te staan
-                                    } else {
-                                        header("location: form.php?choise={$formtype}&error=message");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen message hebt ingevuld
-                                    } 
-                                } else {
-                                    header("location: form.php?choise={$formtype}&error=location");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen locatie hebt ingevuld
-                                } 
-                            } else {
-                                header("location: form.php?choise={$formtype}&error=education");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen opleiding hebt ingevuld
-                            } 
-                        }else {
-                            header("location: form.php?choise={$formtype}&error=sort");//wanneer je formulier verzendt,komt de errormessage tevoorschijn wanneer je geen soortformulier hebt ingevuld
-                        }
-                    }
-                    ?>
+                        <?=$succesText?>
                     </div>
                 </div>
             </div>
